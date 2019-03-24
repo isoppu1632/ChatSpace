5c63ef85a73e78ce6cc01728c80054183bb3bcc1
isoppu1632 <owner@MA4-0026.local>
2019-03-24 20:23:08 +0900

    Create

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+  def index
+    @user =User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
   def edit
-
   end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
